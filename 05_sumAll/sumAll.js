b387c74761681e77c73027047bbc8cb0a232925e
IsThatTheLegend27 <154406381+IsThatTheLegend27@users.noreply.github.com>
2024-01-10 18:06:02 +0800

    Completed the sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total= 0;
+    let min = num1;
+    let max = num2;
 
+    if (!Number.isInteger(min) || !Number.isInteger(max))
+        return ("ERROR");
+
+    if (min <0 || max <0)
+        return ("ERROR");
+
+    if (num2 < num1)
+    {
+        max = num1;
+        min = num2;
+    }
+
+    for (min; min<=max; min++)
+    {
+        total += min;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
