c7643ab5c382caea6b308dd4b81fb34775f4b65f
IsThatTheLegend27 <154406381+IsThatTheLegend27@users.noreply.github.com>
2024-01-10 18:19:33 +0800

    Completed the logic of calculating leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%4 == 0 && (year%100 != 0 || year %400 == 0))
+        return true;
+    else
+        return false;
 };
+
+// could be shorten by writing
+// return year%4 == 0 && (year%100 != 0 || year %400 == 0)
 
 // Do not edit below this line
 module.exports = leapYears;
